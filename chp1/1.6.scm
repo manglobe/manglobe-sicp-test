178ef1552bc933cca4d220fcc81294cefe30ea32
manglobe <manglobegit@gmail.com>
2019-03-20 16:54:02 +0800

    1.6

---
+++ chp1/1.6.scm
@@ -0,0 +1,3 @@
+;page 12 (if <predicate> <consequent> <alternative>)
+;normal-if -> (true? <prodicate> (run <consequent>)) 
+;new-if -> 应用序求值 -> (new-if <predicate> (run <consequent>) (run <alternative>))
