326d1ed656b35401ac541130d4cd5912a6ecadfc
manglobe <manglobegit@gmail.com>
2019-09-18 16:10:09 +0800

    1.32.scm

---
+++ chp1/1.32.scm
@@ -0,0 +1,25 @@
+; (a)
+(define (accumulate combiner null-value term a next b)
+    (define (iter a result)
+        (if (> a b)
+            result
+            (iter (next a) 
+                (combiner (term a)
+                    result
+                )
+            )
+        )
+    )
+    (iter a null-value)
+)
+
+; (b)
+
+(define (accumulate combiner null-value term a next b)
+    (if (> a b)
+        null-value
+        (combiner (term a)
+            (accumulate term (next a) next b)
+        )
+    )
+)
