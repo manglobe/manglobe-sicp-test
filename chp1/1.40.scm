4fac0377a0e77a1411abf18a1d7835d93fee8599
manglobe <manglobegit@gmail.com>
2019-11-18 11:50:22 +0800

    1.40.scm

---
+++ chp1/1.40.scm
@@ -0,0 +1,36 @@
+(load "./1.36.scm")
+(define dx 0.00001)
+
+(define (deriv g)
+  (lambda (x) 
+    (/ (- (g (+ x dx)) (g x))
+      dx
+    )
+  )  
+)
+
+(define (newton-transform g)
+  (lambda (x) 
+    (- x 
+      (/ (g x) ((deriv g) x))  
+    )
+  )  
+)
+
+(define (newton-method g guess)
+  (fixed-point (newton-transform g) guess)
+)
+
+
+(define (cubic a b c)
+  (lambda (x)
+    (+ (* x x x) 
+      (* a x x)
+      (* b x)
+      c
+    )
+  )
+)
+(define (main a b c)
+  (newton-method (cubic a b c) 1)  
+)
