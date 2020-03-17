f550a343b8de559492c17ef661ca4c267290edf5
studio_4 <studio4@foxmail.com>
2020-03-17 18:11:28 +0800

    2.36 2.37

---
+++ chp2/2.36.scm
@@ -0,0 +1,13 @@
+(load "2.33.scm")
+(define (accumulate-n op init seqs)
+    (if (null? (car seqs))
+        '()
+        (cons (accumulate op init (map (lambda (sub-seqs) (car sub-seqs)) seqs))
+            (accumulate-n op init (map (lambda (sub-seqs) (cdr sub-seqs)) seqs))
+        )
+    )
+)
+
+(define (main)
+    (accumulate-n + 0 '((1 2 3) (4 5 6) (7 8 9) (10 11 12)))
+)
