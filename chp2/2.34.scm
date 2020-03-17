6ebca9673767c484c3f373f38e582315bc86eab4
studio_4 <studio4@foxmail.com>
2020-03-17 16:02:30 +0800

    2.34

---
+++ chp2/2.34.scm
@@ -0,0 +1,21 @@
+(load "./2.33.scm")
+(define (horner-eval n list)
+    (accumulate 
+        (lambda (x y) 
+            (+  x 
+                (* n 
+                    (if (null? (cdr list))
+                        0
+                        (horner-eval n (cdr list) )
+                    )
+                )
+            )
+        ) 
+        0 list
+    )
+)
+
+; x=>1+3x+5x^3+x^5
+(define (main)
+  (horner-eval 2 (list 1 3 0 5 0 1))
+)
